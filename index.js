b05415abbdcd2cdc7905f279ef012fcd9c6a69f7
Vamoss <quissama@gmail.com>
2024-10-08 01:36:38 -0300

    update to new chatgpt model and to the right openai library

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import fetch from "node-fetch";
-import { ChatGPTAPI } from 'chatgpt';
+import OpenAI from 'openai';
 import * as dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -10,21 +9,9 @@ import htmlExpress from 'html-express-js';
 
 dotenv.config();
 
-const api = new ChatGPTAPI({
-    apiKey: process.env.OPEN_API_KEY,
-    completionParams: {
-      // override this depending on the ChatGPT model you want to use
-      // NOTE: if you are on a paid plan, you can't use the free model and vice-versa
-      model: 'text-davinci-003', // free, default model
-      // model: 'text-davinci-002-render', // free, default model
-      // model: 'text-davinci-002-render-sha', // paid, default model (turbo)
-      // model: 'text-davinci-002-render-paid', // paid, legacy model
-      temperature: 0.6, //diversidade que ela fala das coisas
-      max_tokens: 140, //maximo de caracteres
-    },
-    debug: false,
-    fetch: fetch
-})
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -56,24 +43,28 @@ app.get('/zh', (req, res, next) => res.render('index', i18n.getCatalog("zh")));
 app.use(express.static('public'));
 
 app.post('/api', async (req, res) => {
-    try {
-        var context = {};
-        if(req.body.parentId)
-            context.parentMessageId = req.body.parentId;
-        if(req.body.conversationId)
-            context.conversationId = req.body.conversationId;
-        const res2 = await api.sendMessage(req.body.ask, context);
-        
-        console.log("Q: " + req.body.ask);
-        console.log("A: " + res2.text);
+  try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        temperature: 0.6, //diversidade que ela fala das coisas
+        max_tokens: 140, //maximo de caracteres
+        messages: [
+          {role: "user", content: req.body.ask},
+        ]
+      });
+      const content = response.choices[0].message.content;
 
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ text: res2.text, conversationId: res2.conversationId, id: res2.id, status: true }));
-    } catch (exceptionVar) {
-        console.error(exceptionVar);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ text: exceptionVar, status: false }));
-    }
+
+      console.log("Q: " + req.body.ask);
+      console.log("A: " + content);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ text: content, conversationId: 0, id: 0, status: true }));
+  } catch (exceptionVar) {
+      console.error(exceptionVar);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ text: exceptionVar, status: false }));
+  }
 })
 
 app.listen(3000)
